2307b24dbc3944212d1f069319ec541e101a382f
Shulin Ke <59651379+ksl20200108@users.noreply.github.com>
2020-07-16 10:03:30 +0800

    Add files via upload

--- random_distribution2.py
+++ random_distribution2.py
@@ -26,14 +26,26 @@ for i in range(1, len(distribution)):    # calculate cdf
     distribution[i] = [distribution[i][0], distribution[i][1]+distribution[i-1][1]]
     # print(distribution[i-1])
 
-index1 = 0
-random_number = random.uniform(0, distribution[len(distribution)-1][1])
-for i in distribution:
-    if random_number >= i[1]:
-        index1 = distribution.index(i)
-        break
 
-result1 = random.uniform(distribution[index1-1][0], distribution[index1][0])
-print("the result is :", result1)
+def fee1():
+    index1 = 0
+    random_number = random.uniform(0, distribution[len(distribution)-1][1])
+    for i in distribution:
+        if random_number <= i[1]:
+            index1 = distribution.index(i)
+            break
+
+    result1 = random.uniform(distribution[index1-1][0], distribution[index1][0])
+    return result1
+    # print("the result is :", result1)
+
+for i in range(1, 51):
+    fees = []
+    for j in range(1, 12):
+         pi = random.uniform(0, 1)
+         if pi <= 0.95:
+             fees.append(fee1())
+    print("No.", i, " ", fees)
+    print("")
 
 # print(line[:17], "|| ", line[18:])
