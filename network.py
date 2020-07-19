e3cd1a6a172e8195327b964e9c13f66af7d1851b
Shulin Ke <59651379+ksl20200108@users.noreply.github.com>
2020-07-19 15:44:03 +0800

    Update network.py

--- network.py
+++ network.py
@@ -402,7 +402,7 @@ class PeerServer(Singleton):
             log.info("-------------")     # 7.8 find it very important
             for node in nodes:
                 if node not in self.nodes:
-                    log.info("------------nodes_find: nodes ip: " + node.ip + "------------")   # 7.8
+                    log.info("------------nodes_find: " + node.ip + "------------")   # 7.8
                     ip = node.ip
                     port = node.port
                     if local_ip == ip:
