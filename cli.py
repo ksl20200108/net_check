1b4a612a517d521036514797ea8b3dd178edb251
Shulin Ke <59651379+ksl20200108@users.noreply.github.com>
2020-07-24 08:29:56 +0800

    Add files via upload

--- cli.py
+++ cli.py
@@ -143,6 +143,61 @@ def start():    # wait : thread add_block(txs)   txs = []   packing function >1M
     bc = BlockChain()   # only one blockchain called bc
     utxo_set = UTXOSet()
     utxo_set.reindex(bc)
+    couch = couchdb.Server("http://127.0.0.1:5984")
+    try:
+        couch.delete('block_chain')
+    db1 = couch.create('block_chain')
+    doc1 = {
+        "index": 0,
+        "value": 50,
+        "pub_key_hash": "1EiVGWYsWiM7shgR5i9KTE2kUjzqcyQU9W"
+    }
+    db1.create(doc1)
+    doc2 = {
+        "_id": "UTXOl",
+        "height": 0
+    }
+    db1.create(doc2)
+    doc3 = {
+        "_id": "fa2db3113d4598834fcc43719ce613862606588f08ab2d62528ad1204f65a693",
+        "block_header": {
+            "height": 0,
+            "timestamp": "1595317443.8917115",
+            "nonce": "",
+            "hash": "fa2db3113d4598834fcc43719ce613862606588f08ab2d62528ad1204f65a693",
+            "prev_block_hash": "",
+            "hash_merkle_root": "0b3b993e4e22ffe31624c73c009797f0e7fde2b136d0f4c621bf04382d9e08b2"
+        }
+        "magic_no": 3166485692,
+        "transactions": [
+            {
+                "txid": "765864a253859a921dc3a8b8d0cc15c6953c9db9d7fb9bf2119bf7c524073fed",
+                "generation_time": 1595317443.8886538,
+                "fee_size_ratio": 0.25510204081632654,
+                "amount": 50,
+                "vins": [
+                    {
+                        "txid": "",
+                        "pub_key": "1595317443.8886075",
+                        "signature": "",
+                        "vout": -1
+                        }
+                    ],
+                "vouts": [
+                    {
+                        "value": 50,
+                        "pub_key_hash": "1EiVGWYsWiM7shgR5i9KTE2kUjzqcyQU9W"
+                        }
+                    ]
+            }
+        ]
+    }
+    db1.create(doc3)
+    doc4 = {
+        "_id": "l",
+        "hash": "fa2db3113d4598834fcc43719ce613862606588f08ab2d62528ad1204f65a693"
+    }
+    db1.create(doc4)
 
     tcpserver = TCPServer()
     tcpserver.listen()
