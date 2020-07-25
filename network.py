101fa04ab1d2a8cbf6b90db122384e06243375f8
Shulin Ke <59651379+ksl20200108@users.noreply.github.com>
2020-07-25 17:58:21 +0800

    Add files via upload

--- network.py
+++ network.py
@@ -587,7 +587,7 @@ class PeerServer(Singleton):
         if not hasattr(self, "ips"):
             self.ips = []
 
-    def get_ip(self, ifname='enp2s0'):  # 7.13
+    def get_ip(self, ifname='ens33'):  # enp2s0
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         return socket.inet_ntoa(
             fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s', bytes(ifname[:15], 'utf-8')))[20:24])
