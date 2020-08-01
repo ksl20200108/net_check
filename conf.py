e15d54d653e018a508488f9cd94b8169e4baa22a
Shulin Ke <59651379+ksl20200108@users.noreply.github.com>
2020-08-01 22:45:31 +0800

    Add files via upload

--- conf.py
+++ conf.py
@@ -1,6 +1,6 @@
 # coding:utf-8
 
-db_url = "http://127.0.0.1:5984"
-bootstrap_host = "192.168.57.141" # 10.110.146.228
+db_url = "http://couchdb1:5984"
+bootstrap_host = "192.168.118.131"
 bootstrap_port = 5678
 listen_port = 5678
---
+++ conf1.py
@@ -0,0 +1,6 @@
+# coding:utf-8
+
+db_url = "http://couchdb2:5985"
+bootstrap_host = "192.168.118.132"
+bootstrap_port = 5678
+listen_port = 5678
